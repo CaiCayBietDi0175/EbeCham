ea614e66061274253c7eb07d094cc5934260dd54
Bao_walkingtree_0175 <91552852+CaiCayBietDi0175@users.noreply.github.com>
2023-10-20 18:07:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bé Cham';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100008286165286';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Hi em bé';
+const contentCard = 'Chúc em bé của anh 20/10 vui vẻ và hạnh phúc';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
